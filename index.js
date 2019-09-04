e4b846abb8adf150c3a259c1a10baa26166048cb
Antonio <tonoezep@gmail.com>
2019-09-03 21:12:24 -0300

    asd

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ Item = mongoose.model("item", ItemSchema);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(bodyParser.urlencoded({ extended: true }));
